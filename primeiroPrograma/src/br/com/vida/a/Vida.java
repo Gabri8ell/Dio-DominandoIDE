49fadaac656f8397d81fa180c8aeb7031f8af921
Gabri8ell <gbr192008@hotmail.com>
2021-12-28 23:16:17 -0300

    Primeiro commit

---
+++ primeiroPrograma/src/br/com/vida/a/Vida.java
@@ -0,0 +1,14 @@
+package br.com.vida.a;
+
+import br.com.vida.animal.Gato;
+
+public class Vida {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Gato gato = new Gato(" Miau ", " Preto ", 9);
+		System.out.println(gato.toString());
+		
+	}
+
+}
